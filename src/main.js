47e52687b87cb2983377ff7fb2cfc2eb24fe4409
David Mayfield <davidmayfield1818@yahoo.com>
2021-04-15 16:31:04 -0700

    commented in the plan

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-console.log("Rocket Patrol");
+// ----------------------------------------------------------------------------
+// David Mayfield
+// Modded Rocket Patrol WiP
+// 4/18/2020
+// duration:
+// ----------------------------------------------------------------------------
+// point break down
+// New Weapon (20)
+// Additional time per hit (20)
+// Simultaneous two player mode (30)
+// Time remaining display (10)
+// control post launch (5)
+// randomized enemy ship movement (5)
+// increased speed post 30 (5)
+// ... + more idk need (5) currently
+
+console.log("Rocket Patrol Modded");
 
 // game configs
 let config = {
